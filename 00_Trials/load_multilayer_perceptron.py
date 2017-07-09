cc8d1624f84c9e191aeceb5aaf81e2f68fd48895
barde499 <gianluigi.bardelloni@kpn.com>
2017-07-10 01:20:25 +0200

    added prep exercises and start 2nd assignment

---
+++ 00_Trials/load_multilayer_perceptron.py
@@ -0,0 +1,14 @@
+import tensorflow as tf
+
+save_file = './train_model.ckpt'
+saver = tf.train.Saver()
+
+# Launch the graph
+with tf.Session() as sess:
+    saver.restore(sess, save_file)
+
+    test_accuracy = sess.run(
+        accuracy,
+        feed_dict={features: mnist.test.images, labels: mnist.test.labels})
+
+print('Test Accuracy: {}'.format(test_accuracy))
